6bd295af773b97a4f52098cdd87df227eb349697
Branci95 <33682550+Branci95@users.noreply.github.com>
2017-11-16 14:51:24 +0100

    BB

--- SyslogClient/Program.cs
+++ SyslogClient/Program.cs
@@ -12,7 +12,7 @@ namespace SyslogClient
         {
             WCFComponent_1 wcfc1 = new WCFComponent_1();
             WCFComponent_2 wcfc2 = new WCFComponent_2();
-            Console.WriteLine("Branci was here..");
+            Console.WriteLine("Branci was here!!!!!");
             Console.ReadLine();
         }
     }
