0b1164a82addc3322e256c8bfe2ce3413d50c733
babic95 <babic95@gmail.com>
2017-11-17 17:26:12 +0100

    ...

--- SyslogClient/WCFComponent_1.cs
+++ SyslogClient/WCFComponent_1.cs
@@ -8,6 +8,8 @@ using System.Xml.Serialization;
 using System.IO;
 using Common;
 using System.Threading;
+using System.ServiceModel.Description;
+using System.IdentityModel.Policy;
 
 namespace SyslogClient
 {
@@ -20,6 +22,15 @@ namespace SyslogClient
 
             ServiceHost host = new ServiceHost(typeof(Services));
             host.AddServiceEndpoint(typeof(IServices), binding, address);
+
+            host.Authorization.ServiceAuthorizationManager = new CustomAuthorizationManager();
+
+            List<IAuthorizationPolicy> policies = new List<IAuthorizationPolicy>();
+            policies.Add(new CustomAuthorizationPolicy());
+            host.Authorization.ExternalAuthorizationPolicies = policies.AsReadOnly();
+
+            host.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
+
             host.Open();
         }
 
